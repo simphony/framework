c5eb81cf57eee732bde3453969361d6fd1e7e3e1
enthought <enthought@simphony-base.(none)>
2015-05-20 12:46:20 +0100

    remove downloaded file

--- fix_pip.sh
+++ fix_pip.sh
@@ -3,6 +3,7 @@ set -e
 
 wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 python get-pip.py
+rm get-pip.py
 pip --version
 pip install --upgrade setuptools
 pip install --upgrade virtualenv
