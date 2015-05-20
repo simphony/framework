66fd4439af12725fbd9129554eef79d2f18978bc
enthought <enthought@simphony-base.(none)>
2015-05-20 11:33:20 +0100

    use wget

--- fix_pip.sh
+++ fix_pip.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-curl https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+wget https://raw.github.com/pypa/pip/master/contrib/get-pip.py
 python get-pip.py
 pip --version
 pip install --upgrade setuptools
